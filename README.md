a83b4787d7c1fda09928bd89cb892be61ae9c05c
codisan <merrich.chen@jwis.cn>
2017-12-17 20:53:31 +0800

    x

---
+++ README.md
@@ -0,0 +1,58 @@
+# 无配置ORM框架
+
+## 定义model
+``` java
+@Table(name="user")
+public class User extends Dao {
+	@Column(name = "user_id", primaryKey=true, autoIncrement=true)
+	public Integer userId;
+	
+	@Column(name = "email_id")
+	public String emailId;
+	
+	public String password;
+	
+	@Column(name = "last_name")
+	public String lastName;
+	
+	@Column(name = "first_name")
+	public String firstName;
+	
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", emailId=" + emailId + ", password=" + password + ", firstName=" + firstName
+				+ ", lastName=" + lastName + "]";
+	}	
+}
+```
+
+## 单对象查询
+``` java
+User u = Dao.findOne(User.class, "where user_id = 2");
+System.out.println(u);
+```
+
+## 新增
+``` java
+User u = new User();
+u.firstName = "xx";
+u.lastName = "oo";
+u.emailId = "xx@oo.com";
+u.password = "xx pass word";
+		
+int row = Dao.insert(User.class, u);
+```
+
+## 更新
+``` java
+User u = Dao.findOne(User.class, "where user_id = 2");
+u.firstName = "333333333333";
+u.update(User.class, u);
+```
+
+## 删除
+``` java
+User u = Dao.findOne(User.class, "where user_id = 6");
+u.destroy(User.class, u);
+```
+
--- src/test/java/com/codisan/no_conf_orm/Reflect.java
+++ src/test/java/com/codisan/no_conf_orm/Reflect.java
@@ -1,50 +1,39 @@
 package com.codisan.no_conf_orm;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.sql.SQLException;
 
 import org.junit.Test;
 
 public class Reflect {
-
 	@Test
-	public void testConstructor() {
-		// User u = new User();
-		
-		try {
-			// find one
-			 User u = Dao.findOne(User.class, "where user_id = 1");
-			 System.out.println(u);
-			 
-			 u.firstName = "--------------";
-			 
-			 // u.update(User.class, u);
-			 
-			 u.destroy(User.class, u);
-			
-			// insert
-//			User u = new User();
-//			u.firstName = "xx";
-//			u.lastName = "oo";
-//			u.emailId = "xx@oo.com";
-//			u.password = "xx pass word";
-//			
-//			int row = Dao.insert(User.class, u);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-				
-		// Class 
+	public void select() throws InstantiationException, IllegalAccessException, NoSuchFieldException, SecurityException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException, SQLException {
+		User u = Dao.findOne(User.class, "where user_id = 2");
+		System.out.println(u);		
 	}
 	
-	
 	@Test
-	public void xx() {
-//		User u = new User();
-//		Method[] methods = User.class.getMethods();
-//
-//		for(Method method : methods){
-//			System.out.println("method = " + method.getName());
-//		}
+	public void insert() throws IllegalArgumentException, IllegalAccessException, SQLException {
+		User u = new User();
+		u.firstName = "xx";
+		u.lastName = "oo";
+		u.emailId = "xx@oo.com";
+		u.password = "xx pass word";
+		
+		int row = Dao.insert(User.class, u);
+	}
+	
+	@Test 
+	public void update() throws InstantiationException, IllegalAccessException, NoSuchFieldException, SecurityException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException, SQLException {
+		User u = Dao.findOne(User.class, "where user_id = 2");
+		u.firstName = "333333333333";
+		u.update(User.class, u);
+	}
+	
+	@Test 
+	public void destroy() throws InstantiationException, IllegalAccessException, NoSuchFieldException, SecurityException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException, SQLException {
+		User u = Dao.findOne(User.class, "where user_id = 6");
+		u.destroy(User.class, u);
 	}
 }
